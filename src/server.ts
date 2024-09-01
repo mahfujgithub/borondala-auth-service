20156f8ad80bf4b697fb7f602920e6b9a5c91ae3
mahfujgithub <mahfujurrahmanofficial23@gmail.com>
2024-09-02 05:52:01 +0600

    eslint, prettier setup

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,19 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./config"
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config'
 
 async function boostrap() {
   try {
-    await mongoose.connect(config.database_url as string);
-    
-    console.log(`Database is connected successfully!`);
+    await mongoose.connect(config.database_url as string)
+
+    console.log(`Database is connected successfully!`)
 
     app.listen(config.port, () => {
-      console.log(`App listening on port ${config.port}`);
-    });
+      console.log(`App listening on port ${config.port}`)
+    })
   } catch (err) {
-    console.log(`failed to connect database, ${err}`);
+    console.log(`failed to connect database, ${err}`)
   }
 }
 
-boostrap();
+boostrap()
