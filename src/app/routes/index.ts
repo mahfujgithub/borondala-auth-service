c63387ee1a2bdc9e88687e5d29407d48b161ea29
mahfujgithub <mahfujurrahmanofficial23@gmail.com>
2024-10-27 20:12:00 +0600

    extra routes removed

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { UserRoutes } from '../modules/users/user.route';
 import { CustomerRoutes } from '../modules/customer/customer.route';
-import { DalaRoutes } from '../modules/products/ProductItems/GiftDala/dala.route';
 import { AdminRoutes } from '../modules/admin/admin.route';
 import { AuthRoutes } from '../modules/auth/auth.route';
 const router = express.Router();
@@ -18,15 +17,7 @@ const moduleRoutes = [
   {
     path: '/auth',
     route: AuthRoutes,
-  },
-  {
-    path: '/watch',
-    route: DalaRoutes,
-  },
-  {
-    path: '/dala',
-    route: DalaRoutes,
-  },
+  }
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
